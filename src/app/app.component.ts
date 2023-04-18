2120b1de5f349339fcfb72f4b792e782afa690fd
AlexandruBirta <alexbirta2011@gmail.com>
2023-04-18 20:01:20 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'expense-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'expense-tracker-frontend';
+}
